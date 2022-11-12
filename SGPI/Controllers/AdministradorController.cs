c6e9ded990da53bb27b630f179fb09a796847e6d
JF <JF@DESKTOP-GQGK3TI>
2022-11-12 18:58:21 -0500

    Agregar archivos de proyecto.

---
+++ SGPI/Controllers/AdministradorController.cs
@@ -0,0 +1,193 @@
+﻿    using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SGPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGPI.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        SGPDBContext context;
+
+        public AdministradorController(SGPDBContext contexto)
+        {
+            context = contexto;
+        }
+
+
+        public ActionResult MenuAdministrador()
+        {
+            return View();
+        }
+
+
+        public IActionResult CrearUsuario()
+        {
+            ViewBag.tipodoc = context.Documentos.ToList();
+            ViewBag.programa = context.Programas.ToList();
+            ViewBag.rol = context.Rols.ToList();
+            ViewBag.genero = context.Generos.ToList();
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult CrearUsuario(Usuario user)
+        {
+            context.Add(user);
+            context.SaveChanges();
+            ViewBag.tipodoc = context.Documentos.ToList();
+            ViewBag.programa = context.Programas.ToList();
+            ViewBag.rol = context.Rols.ToList();
+            ViewBag.genero = context.Generos.ToList();
+            return RedirectToAction("CrearUsuario");
+        }
+
+    
+        public IActionResult BuscarUsuario()
+        {
+            var listaUsuarios = context.Usuarios.ToList();
+            var listaprogram = context.Programas.ToList();
+            List<string> listaprogramas = new List<string>();
+            foreach (var user in listaUsuarios)
+            {
+                if(user.IdPrograma != null)
+                {
+                    foreach (var programa in listaprogram)
+                    {
+                        if (user.IdPrograma == programa.IdPrograma)
+                        {
+                            listaprogramas.Add(programa.ValPrograma);
+                        }
+                    }
+                }
+                else
+                {
+                    listaprogramas.Add("no programa");
+                }
+                
+            }
+            ViewBag.programa = listaprogramas;
+            return View(listaUsuarios);
+        }
+
+        [HttpPost]
+        public IActionResult BuscarUsuario(string documento)
+        {
+            var listaUsuarios = context.Usuarios.Where(u => u.Documento.Contains(documento)).ToList();
+            var listaprogram = context.Programas.ToList();
+            List<string> listaprogramas = new List<string>();
+            foreach (var user in listaUsuarios)
+            {
+                if (user.IdPrograma != null)
+                {
+                    foreach (var programa in listaprogram)
+                    {
+                        if (user.IdPrograma == programa.IdPrograma)
+                        {
+                            listaprogramas.Add(programa.ValPrograma);
+                        }
+                    }
+                }
+                else
+                {
+                    listaprogramas.Add("no programa");
+                }
+
+            }
+            ViewBag.programa = listaprogramas;
+            if (listaUsuarios != null)
+            {
+                return View(listaUsuarios);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult Informes()
+        {
+            var listaUsuarios = context.Usuarios.ToList();
+            var listaprogram = context.Programas.ToList();
+            List<string> listaprogramas = new List<string>();
+            foreach (var user in listaUsuarios)
+            {
+                if (user.IdPrograma != null)
+                {
+                    foreach (var programa in listaprogram)
+                    {
+                        if (user.IdPrograma == programa.IdPrograma)
+                        {
+                            listaprogramas.Add(programa.ValPrograma);
+                        }
+                    }
+                }
+                else
+                {
+                    listaprogramas.Add("no programa");
+                }
+
+            }
+            ViewBag.programa = listaprogramas;
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            ViewBag.tipodoc = context.Documentos.ToList();
+            ViewBag.programa = context.Programas.ToList();
+            ViewBag.rol = context.Rols.ToList();
+            ViewBag.genero = context.Generos.ToList();
+            var listaUsuarios = context.Usuarios.Where(u => u.IdUsuario == id).ToList();
+            if (listaUsuarios != null)
+            {
+                return View(listaUsuarios.SingleOrDefault());
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Usuario user, int id)
+        {
+            Usuario usuario = user;
+            if (usuario == null)
+                return ViewBag.mensaje = "Eror al editar usuario";
+            else
+            {
+                user.IdUsuario = id;
+                context.Update(user);
+                context.SaveChanges();
+                ViewBag.tipodoc = context.Documentos.ToList();
+                ViewBag.programa = context.Programas.ToList();
+                ViewBag.rol = context.Rols.ToList();
+                ViewBag.genero = context.Generos.ToList();
+            }
+
+            return RedirectToAction("BuscarUsuario");
+        }
+
+
+        public ActionResult Delete(Usuario user, int id)
+        {
+            Usuario usuario = context.Usuarios.Find(id);
+            if (usuario == null)
+                return ViewBag.mensaje = "Eror al editar usuario";
+            else
+            {
+                //user.IdUsuario = id;
+                context.Remove(usuario);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("BuscarUsuario");
+        }
+    }
+}
---
+++ SGPI/Controllers/LoginController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SGPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SGPI.Controllers
+{
+    public class LoginController : Controller
+    {
+        SGPDBContext context;
+
+        public LoginController(SGPDBContext contexto)
+        {
+            context = contexto;
+        }
+
+      
+        public ActionResult Login()
+        {
+            bool mensaje = false;
+            return View(mensaje);
+        }
+
+        [HttpPost]
+        public ActionResult Login(string documento,string contracena)
+        {
+            var usuario = context.Usuarios.Where(u => u.Documento == documento && u.Contraseña == contracena).ToList();
+            try
+            {
+                if (usuario[0].IdRol == 1)
+                {
+                    return Redirect("/Administrador/CrearUsuario");
+                }
+                else if (usuario[0].IdRol == 2)
+                {
+                    return Redirect("/Coordinador/MenuCoordinador");
+                }
+                else if (usuario[0].IdRol == 3)
+                {
+                    return Redirect("/Estudiante/MenuEstudiante");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch(Exception e)
+            {
+                return View();
+            }
+            
+        }
+
+        
+        public ActionResult OlvidoContracena()
+        {
+            return View();
+        }
+
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
